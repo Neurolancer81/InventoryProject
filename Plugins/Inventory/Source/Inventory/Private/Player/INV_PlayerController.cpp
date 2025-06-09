739ad18a09143efc38594dac1f02def1ba256318
Neurolancer81 <87434607+Neurolancer81@users.noreply.github.com>
2025-06-08 21:59:44 -0400

    Created Plugin

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/INV_PlayerController.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/INV_PlayerController.h"
+#include "Inventory.h"
+
+void AINV_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogInventory, Log, TEXT("AINV_PlayerController::BeginPlay"));	
+}
